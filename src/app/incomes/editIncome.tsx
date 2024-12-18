762f48c136a5424192bbd21babe3451ff00eebde
cagri <cagri.gunal@gmail.com>
2024-12-18 10:54:01 +0300

    updated editIncome and editExpense to show user friendly number input

--- src/app/incomes/editIncome.tsx
+++ src/app/incomes/editIncome.tsx
@@ -7,8 +7,8 @@ import { useExpense } from '@/app/context/store'
 import { Dialog, DialogActions, DialogBody, DialogTitle } from '@/components/dialog'
 import { Button } from '@/components/button'
 import { FieldGroup, Field, Label } from '@/components/fieldset'
-import { IncomeCategory } from '../types'
 import { IncomeItem } from '../types'
+import { unformat, InputNumberFormat } from '@react-input/number-format'
 
 export function EditIncome({ incomeToEdit }: { incomeToEdit: IncomeItem }) {
   const { incomeCategories, updateIncomeItem } = useExpense()
@@ -21,7 +21,7 @@ export function EditIncome({ incomeToEdit }: { incomeToEdit: IncomeItem }) {
     updateIncomeItem({
       ...incomeToEdit,
       description: (e.target as any).description.value,
-      amount: Number((e.target as any).amount.value),
+      amount: Number(unformat((e.target as any).amount.value, 'tr-TR')),
       category: incomeCategories.find(c => c.id === selectedCategory) || incomeToEdit.category,
       date: (e.target as any).date.value,
     })
@@ -46,7 +46,14 @@ export function EditIncome({ incomeToEdit }: { incomeToEdit: IncomeItem }) {
               </Field>
               <Field>
                 <Label>Amount</Label>
-                <Input name="amount" defaultValue={incomeToEdit.amount} placeholder="$0.00" />
+                <InputNumberFormat 
+                name="amount" 
+                className='relative block w-full appearance-none rounded-lg px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] text-base/6 text-zinc-950 placeholder:text-zinc-500 sm:text-sm/6 dark:text-white border border-zinc-950/10 data-[hover]:border-zinc-950/20 dark:border-white/10 dark:data-[hover]:border-white/20 bg-transparent dark:bg-white/5 focus:outline-none data-[invalid]:border-red-500 data-[invalid]:data-[hover]:border-red-500 data-[invalid]:dark:border-red-600 data-[invalid]:data-[hover]:dark:border-red-600 disabled:border-zinc-950/20 disabled:dark:border-white/15 disabled:dark:bg-white/[2.5%] dark:data-[hover]:disabled:border-white/15'
+                locales="tr-TR"
+                format="currency"
+                currency="TRY"
+                defaultValue={incomeToEdit.amount} 
+                placeholder="0.00" />
               </Field>
               <Field>
                 <Label>Category</Label>
